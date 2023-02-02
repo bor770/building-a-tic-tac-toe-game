c9a00ff8922c3f95680ed33cc1620b3902c5d169
Baruch Giverts <bor770@gmail.com>
2023-02-02 09:44:48 +0200

    in middle

---
+++ my/scripts/elements.js
@@ -0,0 +1,10 @@
+const editPlayer1ButtonElement = document.querySelector(`#editPlayer1Button`);
+const editPlayer2ButtonElement = document.querySelector(`#editPlayer2Button`);
+
+const startButtonElement = document.querySelector(`#startButton`);
+
+const playerNameDialogElement = document.querySelector(`#playerNameDialog`);
+
+const formElement = document.querySelector(`#form`);
+
+const cancelButtonElement = document.querySelector(`#cancelButton`);
